79e089f1855d881ebe6a9cea5fa17aeed99b9196
Rod Sot <hi@rodsot.com>
2020-06-15 19:15:01 -0500

    Fix identation

--- 404.php
+++ 404.php
@@ -4,15 +4,13 @@
 	<section class="w70 center-content">
 		<article id="post-404">
 
-            <h1>Error 404</h1>
-
-            <?php get_search_form(); ?>
-
+         <h1>Error 404</h1>
+         <?php get_search_form(); ?>
 			<p><a href="<?php echo home_url(); ?>"><strong>Return to home</strong></a></p>
 
-        </article>
+      </article>
 
-    </section>
+   </section>
 </main>
 
 <?php get_footer(); ?>
