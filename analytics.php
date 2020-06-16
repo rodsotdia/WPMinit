a96d75add00cb8e0a0d85f67a60d73f4ee17318f
Rod Sot <hi@rodsot.com>
2020-06-15 19:16:15 -0500

    Fix identation

--- analytics.php
+++ analytics.php
@@ -1,11 +1,11 @@
 <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXX-X"></script>
 <script>
-  <?php if (current_user_can('administrator') ) : ?>
-    // Disable tracking of Google Analytics for admin users logged in.
-    window['ga-disable-UA-XXXXXX-X'] = true;
-  <?php endif; ?>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-XXXXXX-X');
+   <?php if (current_user_can('administrator') ) : ?>
+      // Disable tracking of Google Analytics for admin users logged in.
+      window['ga-disable-UA-XXXXXX-X'] = true;
+   <?php endif; ?>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+   gtag('config', 'UA-XXXXXX-X');
 </script> -->
