095cd3b9d289b2c013fa156984949bc1e4e1e874
Rod Sot <hi@rodsot.com>
2020-08-25 20:03:43 +1200

    Remove scripts installed by npm

--- assets/js/scripts.php
+++ assets/js/scripts.php
@@ -21,17 +21,6 @@
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script> -->
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script> -->
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/swup@2.0.9/dist/swup.min.js"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/@swup/debug-plugin@1.0.3/dist/SwupDebugPlugin.min.js"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/swup-gtag-plugin@0.0.0/dist/SwupGtagPlugin.min.js"></script> -->
-
-<!-- <script src="https://cdn.jsdelivr.net/npm/bowser@2.9.0/es5.js"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js"></script> -->
-
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js"></script> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"></script> -->
-
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js"></script> -->
 
 <!-- <script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script> -->
