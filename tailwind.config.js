b449bb05a05a65e35d543c105dc9721c41496a21
Rod Sot <hi@rodsot.com>
2020-08-26 16:50:49 +1200

    Add screens to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
       './**/*.php'
    ],
    theme: {
+      screens: {
+         'xsm': '425px',
+         'sm': '640px',
+         'md': '800px',
+         'lg': '1024px',
+         'xl': '1280px',
+         'xxl': '1366px'
+      },
       fontFamily: {
          'DMSans': ['DM Sans', 'sans-serif']
       },
